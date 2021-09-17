d79050ecc0a0212b3256e65f6ba1c06fdaa9805e
Allan V <allanvillegas90@gmail.com>
2021-09-17 14:46:07 +0800

    Exercise 4??

--- 3-nest/src/exercise3/exercise3.controller.ts
+++ 3-nest/src/exercise3/exercise3.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Exercise3Service } from './exercise3.service';
 
 @Controller('exercise3')
@@ -25,4 +25,36 @@ export class Exercise3Controller {
     //     thefucntioncode
     //     return(must match return type);
     // }
+
+  @Get('/car/:id')
+  getOne(@Param("id") id:string) {
+    return this.e3.getCar(id);
+  }
+
+  @Post('/addCar')
+  addCar(@Body() body:any) {
+    return this.e3.addCar(body);
+  }
+
+  @Put('/replaceCar/:id')
+  replaceCar(@Param("id") id:string, @Body() body: any) {
+    return this.e3.replaceCar(id,body);
+  }
+
+  @Delete('/replaceCar/:id')
+  removeCar(@Param("id") id:string) {
+    return this.e3.deleteCar(id);
+  }
+
+  @Get('/addJoshCar2')
+  test2() {
+    return this.e3.addJoshCar2();
+    // return;
+  }
+
+  @Get('/logCars')
+  logCars() {
+    return this.e3.logAllCars();
+    // return;
+  }
 }
